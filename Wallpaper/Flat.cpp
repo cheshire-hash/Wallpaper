853beb2ac2e5e536316a5965ab951db411d939ef
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2025-09-30 23:47:28 +0300

    /

--- Wallpaper/Flat.cpp
+++ Wallpaper/Flat.cpp
@@ -1,1 +1,31 @@
 #include "Flat.h"
+#include <iostream>
+
+Flat::Flat() : list(nullptr) { countroom++; }
+
+Flat::Flat(int numbers)
+{
+	list = new Room[numbers];
+	countroom++;
+}
+
+Flat::Flat(const Flat& obj)
+{
+	if (list != nullptr)
+	{
+		delete[] list;
+	}
+	list = new Room[sizeof(obj.list) / sizeof(obj.list[0])];
+	for (int i = 0; i < sizeof(obj.list) / sizeof(obj.list[0]); i++)
+	{
+		list[i] = obj.list[i];
+	}
+	countroom++;
+}
+
+Flat::~Flat()
+{
+	delete[] list;
+	countroom--;	
+}
+
--- Wallpaper/Flat.h
+++ Wallpaper/Flat.h
@@ -3,5 +3,11 @@
 class Flat
 {
 	Room* list;
+	static int countroom;
+public:
+	Flat();
+	Flat(int n);
+	Flat(const Flat& obj);
+	~Flat();
 };
 
